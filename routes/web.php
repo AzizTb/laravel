ac57b0d83a9d05cc07750973787f701e4495cfdb
AzizTb <raphlust69@gmail.com>
2021-10-04 14:29:06 +0700

    Routing

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,40 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Route::get('1', function () {
+//     return 'satu';
+// });
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('2', function () {
+//     return 'dua';
+// });
+
+// Route::get('3', function () {
+//     return 'tiga';
+// });
+
+// Route::get('4', function () {
+//     return 'empat';
+// });
+
+// Route::get('5', function () {
+//     return 'lima';
+// });
+
+// parameter wajib
+// Route::get('/{nama}/{tahun_lahir}', function ($nama, $tahun_lahir) {
+//     return 'Nama : ' . $nama .'<br>' .'Tahun : ' . $tahun_lahir;
+// });
+
+// parameter opsional
+// Route::get('/{nama?}/{tahun_lahir}', function ($nama, $tahun_lahir = null) {
+//     return 'Nama : ' . $nama .'<br>' .'Tahun : ' . $tahun_lahir;
+// });
+
+
+Route::get('/hitung-luas-segitiga/{alas?}/{tinggi?}', function ($alas = 1, $tinggi = 1) {
+    $luas = $alas * $tinggi / 2;
+    return 'Alas nya : ' . $alas . '<br>'
+    . 'Tingginya : ' . $tinggi . '<br>'
+    . 'Hasil luas segitiga : ' .$luas;
 });
